6f95ecd9944f8d99c26fe9eaf2deb98889c290e2
raffitz <raf.a.m.c.gon@gmail.com>
2021-10-26 11:43:04 +0100

    Add README and demo

---
+++ README.md
@@ -0,0 +1,7 @@
+![Mawkin Sans](title.png)
+
+Mawkin Sans is a self-flipping font.
+
+An example of text can be seen here:
+
+![The last metroid is in captivity. The galaxy is at peace.](demo.png)
--- build.sh
+++ build.sh
@@ -5,3 +5,11 @@ set -e
 psfnormalize mawkin_sans.ufo
 
 fontmake --validate-ufo --autohint --ufo-paths mawkin_sans.ufo --output otf --output-path mawkin_sans.otf
+
+convert -background white -fill black \
+          -font './mawkin_sans.otf' -pointsize 200 label:"Mawkin\n. .Sans" \
+          title.png
+
+convert -background white -fill black \
+          -font './mawkin_sans.otf' -pointsize 60 label:"the last metroid\n. .is in captivity\nthe galaxy\n. .is at peace\n\n\nMawkin\n. .Sans" \
+          demo.png
