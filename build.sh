7166c85d3d16dfe0620203c4caa04581a640fd05
Bryan Elliott <bryan_elliott@comcast.com>
2021-10-27 10:20:55 -0400

    Added docs section, woff2 builder

--- build.sh
+++ build.sh
@@ -13,3 +13,12 @@ convert -background white -fill black \
 convert -background white -fill black \
           -font './mawkin_sans.otf' -pointsize 60 label:"the last metroid\n. .is in captivity\nthe galaxy\n. .is at peace\n\n\nMawkin\n. .Sans" \
           demo.png
+<<<PY
+
+# Convert OTF to WOFF2 for webfont
+python3 <<PY
+from fontTools.ttLib import TTFont
+f = TTFont('mawkin_sans.otf')
+f.flavor='woff2'
+f.save('docs/mawkin_sans.woff2')
+PY
